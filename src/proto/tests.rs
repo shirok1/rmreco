bd5f837a9a23336987941446a24630d4303f1d33
shiroki <me@shirok1.dev>
2023-07-18 05:41:51 +0800

    test: serialize test for `graphic::GraphicData` and `RefereeWarning`

--- src/proto/tests.rs
+++ src/proto/tests.rs
@@ -6,8 +6,10 @@ fn graphic_data_size() {
     let mut data = [0u8; 15];
     for i in 0b000..=0b111 {
         data[3] = i * 0b0000_0100;
-        let ((_, rest_byte_size), _) = graphic::GraphicData::from_bytes((&data[..], 0)).unwrap();
+        let ((_, rest_byte_size), parsed) = graphic::GraphicData::from_bytes((&data[..], 0)).unwrap();
         assert_eq!(rest_byte_size, 0);
+        let bytes: Vec<u8> = parsed.try_into().unwrap();
+        assert_eq!(bytes.as_slice(), data);
     }
 }
 
@@ -16,8 +18,10 @@ fn referee_warning_size() {
     let mut data = [0u8; 2];
     for i in 0b01..=0b11 {
         data[0] = i;
-        let ((_, rest_byte_size), _) = RefereeWarning::from_bytes((&data[..], 0)).unwrap();
+        let ((_, rest_byte_size), parsed) = RefereeWarning::from_bytes((&data[..], 0)).unwrap();
         assert_eq!(rest_byte_size, 0);
+        let bytes: Vec<u8> = parsed.try_into().unwrap();
+        assert_eq!(bytes.as_slice(), data);
     }
 }
 
